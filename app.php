94a71f0c8c87d9d8dd082b193eedeecac973b2e3
diolan12 <dio_lantief21@outlook.com>
2021-03-01 08:38:13 +0700

    Update app.php

--- app.php
+++ app.php
@@ -1,9 +1,21 @@
 <?php
 
+/*
+* Your web app name. This will shown in title bar and header.
+*/
 $appName = "My Theatre";
 
+/*
+* This is the path of your application installation.
+*/
 $path = "videos";
 
+/*
+* This is symlink which will be used to pointing your movies folder anywhere on your disk.
+*/
 $symlink = "movies";
 
+/*
+* You can edit this if you know what are you doing.
+*/
 $baseUrl = "http://" . $_SERVER['SERVER_NAME'] ."/". $path;
